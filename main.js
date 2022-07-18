5a27f6f991af34272ed5703048704b2170f30b55
Gustavo <garancibiacl@gmail.com>
2022-07-18 13:29:09 -0400

    se modifican dimensiones de container calc

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-   const el1 = document.querySelector('.calculator');
+   const calc = document.querySelector('.calculator');
       const btnDark = document.querySelector('.btn--dark');
       btnDark.addEventListener('click' ,function() {
-        if(el1.classList.contains('dark')) {
-          el1.classList.remove('dark');
+        if(calc.classList.contains('dark')) {
+          calc.classList.remove('dark');
           this.classList.remove('btn--light');
           document.querySelector('body').style.background = '#dde1e7';
         }
         else {
-          el1.classList.add('dark');
+          calc.classList.add('dark');
           this.classList.add('btn--light')
           document.querySelector('body').style.background = '#272727';
         }
