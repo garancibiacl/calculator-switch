8cd50f4a7bb96b082920e3721db54560feb042c1
Gustavo <garancibiacl@gmail.com>
2022-07-17 23:42:43 -0400

    cambio height toggle responsive

--- main.js
+++ main.js
@@ -12,3 +12,20 @@
           document.querySelector('body').style.background = '#272727';
         }
       })
+
+
+      $(function(){
+
+
+        // minimize (looks kinda cool?)
+          $(".min").click(function(){
+              $("calculator").stop().animate({
+                  width: "0px", height: "0px", marginLeft: "700px", marginTop: "1000px"
+              }, 500);
+              setTimeout(function(){$(".cal").css("display", "none")}, 600);
+          });
+        //close window. refresh to get back
+          $(".close").click(function(){
+              $(".calculator").css("display", "none");
+          })
+        })
